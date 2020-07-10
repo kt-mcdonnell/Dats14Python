e59bd751a2b8fec649d282aa6427faba92ca85fb
Katie <katie.mcdonnell@hotmail.co.uk>
2020-07-10 11:51:13 +0100

    Created basic commands

---
+++ Introduction/hello_data_types.py
@@ -0,0 +1,31 @@
+
+hw = "Hello World!"
+
+print(len(hw))
+
+#  0 1 2 3 4 5 6 7 8 9 10 11
+#  H e l l o   W o r l d  !
+
+print(hw[6])
+print(hw[3:7])
+print(hw[4:])
+print(hw[:8])
+
+print(hw[-11])
+print(hw[-10:-5])
+print(hw[-7:])
+
+white_space = "       white space       "
+print(len(white_space))
+print(white_space.strip())
+print(white_space.lstrip())
+print(white_space.rstrip())
+print(white_space.count(" "))
+print(white_space.())
+print(white_space.strip())
+print(white_space.strip())
+print(white_space.strip())
+
+
+
+
