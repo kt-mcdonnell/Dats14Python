01f07bf18eb82e9003fe3f2a87af343d74187695
Katie <katie.mcdonnell@hotmail.co.uk>
2020-07-10 13:50:51 +0100

    data types examples

--- Introduction/hello_data_types.py
+++ Introduction/hello_data_types.py
@@ -1,56 +1,69 @@
+# #
+# # hw = "Hello World!"
+# #
+# # print(len(hw))
+# #
+# # #  0 1 2 3 4 5 6 7 8 9 10 11
+# # #  H e l l o   W o r l d  !
+# #
+# # print(hw[6])
+# # print(hw[3:7])
+# # print(hw[4:])
+# # print(hw[:8])
+# #
+# # print(hw[-11])
+# # print(hw[-10:-5])
+# # print(hw[-7:])
+# #
+# # white_space = "       white space       "
+# #
+# # print(len(white_space))
+# # print(white_space.strip())
+# # print(white_space.lstrip())
+# # print(white_space.rstrip())
+# # print(white_space.count(" "))
+# # print(white_space.lower())
+# # print(white_space.upper())
+# # print(white_space.strip().capitalize())
+# # print(white_space.replace("space", "noise"))
+# # print(white_space.strip())
+# #
+# # a = "Aardvark"
+# # b = "Bat"
+# # c = "Cheshire Cat"
+# # d = 5
+# #
+# # print(a + ", " + b + ", " + c + ", " + str(d))
+# #
+# # num = "54"
+# # print(type(int(num)))
+# #
+# # print(f"{a}, {b}, {c}, {d}")
+# #
+# # name = "Katie"
+# # print(f"Hello my name is {name}!")
 #
-# hw = "Hello World!"
 #
-# print(len(hw))
+# name = input("Hi, What is your name?\n")
+# age = int(input(f"Hi {name}! How old are you?\n"))
+# siblings = int(input(f"Wow you are {age}! How many siblings do you have?\n"))
 #
-# #  0 1 2 3 4 5 6 7 8 9 10 11
-# #  H e l l o   W o r l d  !
+# print(f"Cool, {siblings} siblings is fun! What is your favourite decimal number {name}?")
+# dec_num = input()
+# print(f"{dec_num} is my favourite too! What is your favourite animal?")
+# animal = input()
+# print(f"{animal}'s are cool animals!")
 #
-# print(hw[6])
-# print(hw[3:7])
-# print(hw[4:])
-# print(hw[:8])
-#
-# print(hw[-11])
-# print(hw[-10:-5])
-# print(hw[-7:])
-#
-# white_space = "       white space       "
-#
-# print(len(white_space))
-# print(white_space.strip())
-# print(white_space.lstrip())
-# print(white_space.rstrip())
-# print(white_space.count(" "))
-# print(white_space.lower())
-# print(white_space.upper())
-# print(white_space.strip().capitalize())
-# print(white_space.replace("space", "noise"))
-# print(white_space.strip())
-#
-# a = "Aardvark"
-# b = "Bat"
-# c = "Cheshire Cat"
-# d = 5
-#
-# print(a + ", " + b + ", " + c + ", " + str(d))
-#
-# num = "54"
-# print(type(int(num)))
-#
-# print(f"{a}, {b}, {c}, {d}")
-#
-# name = "Katie"
-# print(f"Hello my name is {name}!")
+# print(type(name))
+# print(type(age))
+# print(type(siblings))
+# print(type(dec_num))
+# print(type(animal))
 
-print("Hi, What is your name?")
-name = input()
-print(f"Hi {name}! How old are you?")
-age = input()
-print(f"Wow you are {age}! How many siblings do you have?")
-siblings = input()
-print(f"Cool, {siblings} siblings is fun! What is your favourite decimal number {name}?")
-dec_num = input()
-print(f"{dec_num} is my favourite too! What is your favourite animal?")
-animal = input()
-print(f"{animal}'s are cool animals!")
+hw = "Hello World!"
+
+print(hw.isalpha())
+print(hw.islower())
+print(hw.isupper())
+print(hw.endswith("!"))
+print(hw.startswith("h"))
