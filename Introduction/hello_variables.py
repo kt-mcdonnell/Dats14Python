248ef5d1e8ab334601c45312d12f08c33fded2ea
Katie <katie.mcdonnell@hotmail.co.uk>
2020-07-10 11:41:09 +0100

    Created basic commands

---
+++ Introduction/hello_variables.py
@@ -0,0 +1,28 @@
+# a = 5
+# b = "Hello!"
+# c = 0.5
+# d= True
+# e = (2, 3, 4)
+#
+# print(a)
+# print(b)
+# print(c)
+#
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+
+print("What is your name?")
+name = input()
+print("Hello...")
+print(name)
+print("How old are you " + name + "?")
+age = input()
+print("You are " + age + ", cool! When is your birthday?")
+DOB = input()
+print(DOB + " is a nice birthday!")
+
+
+
