afed88fddeb3603231c5015908397fae08630975
Katie <katie.mcdonnell@hotmail.co.uk>
2020-07-12 23:01:24 +0100

    Added ability to change words fizz and buzz

--- Introduction/fizz_buzz.py
+++ Introduction/fizz_buzz.py
@@ -4,17 +4,20 @@ print('What number shall we start from?')
 start_val = int(input())
 print(f"Cool! {start_val} is a good starting point. What number shall we end on?")
 end_val = int(input())
-
+print(f"Nice! We will finish at {end_val}")
+print('What word should we say instead of multiples of 3?')
+fizz = input().upper()
+print('Lastly what should we say instead of multiples of 5?')
+buzz = input().upper()
 
 counter = start_val
-
 while counter < end_val:
     counter += 1
     if counter % 15 == 0:
-        print('FIZZBUZZ')
+        print(f"{fizz} {buzz}!!!")
     elif counter % 3 == 0:
-        print('FIZZ')
+        print(fizz)
     elif counter % 5 == 0:
-        print('BUZZ')
+        print(buzz)
     else:
         print(counter)
